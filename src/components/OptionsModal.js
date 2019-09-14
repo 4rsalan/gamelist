6b1c867f9306415d9ce43e87033b7f8d9a3f3622
4rsalan <arsalan.farooqui2@georgebrown.ca>
2019-09-13 23:40:41 -0400

    Created custom Modal

---
+++ src/components/OptionsModal.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import {Modal, View, Button, Text, Dimensions} from "react-native";
+
+class OptionalModal extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {modalVisible: this.props.modalVisible}
+    }
+
+    closeModal = ()=>{
+        this.setState({modalVisible:false});
+        this.props.closeModalCallback(false);
+    };
+
+    render() {
+        console.log("Modal Rendered!");
+        return (
+            <Modal
+                animationType="fade"
+                transparent={true}
+                onRequestClose={this.closeModal}
+                visible={this.state.modalVisible}
+            >
+                <View style={{
+                    flex: 1,
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    backgroundColor: '#00000080'
+                }}>
+                    <View  style={{
+                        width: Dimensions.get('window').width * 0.8,
+                        height: Dimensions.get('window').height * 0.6,
+                        backgroundColor: '#fff', padding: 20,
+                    }}>
+                        <Text>{this.props.name || "Name has not been set"}</Text>
+                        <Button
+                            title={"Close Modal"}
+                            onPress={this.closeModal}>
+                        </Button>
+                    </View>
+                </View>
+            </Modal>
+        )
+    };
+}
+
+export default OptionalModal;
